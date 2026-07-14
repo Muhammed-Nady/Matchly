6aa1b429eed47555d6d0231d9b6a8407ec18b751
Mohammed Nady <muhammednadyii@gmail.com>
2026-07-14 09:51:46 +0300

    Finished Section 2

---
+++ Matchly/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Matchly.Data;
+using Matchly.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Matchly.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+    
+}
---
+++ Matchly/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Matchly.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Matchly.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<AppUser> Users { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+    }
+}
