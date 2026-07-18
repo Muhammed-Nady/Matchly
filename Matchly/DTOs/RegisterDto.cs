bb389c90c9b19cd7092cd3327d968e5c8141d527
Mohammed Nady <muhammednadyii@gmail.com>
2026-07-18 18:41:36 +0300

    Applying routing & client side validation on login and routing redirection

--- Matchly/DTOs/RegisterDto.cs
+++ Matchly/DTOs/RegisterDto.cs
@@ -4,9 +4,9 @@ namespace Matchly.DTOs
 {
     public class RegisterDto
     {
-        [Required]
+        [Required(ErrorMessage = "Username must be entered")]
         public string UserName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Password must be entered")]
         public string Password { get; set; }
     }
 }
